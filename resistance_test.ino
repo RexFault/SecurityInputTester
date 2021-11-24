64ad1a03a45b264dcca2d19165db1204c4e18c59
Shane McIntosh <shmcinto@gmail.com>
2021-11-24 14:32:03 -0500

    Added better Ground Fault Detection
    Increased Ground Fault Detection accuracy by using the analog input of the main lead to detect if that lead is grounded.

--- resistance_test.ino
+++ resistance_test.ino
@@ -15,10 +15,17 @@
 
 #define RESISTOR_IN A0
 #define GROUND_LEAD_IN 10
+#define GROUND_LEAD_IN_2 A7
 #define R1 2150 //2.2KOhm resistor metered at 2.1553
 #define MODE_SWITCH_IN 11
 #define MODE_BLUEBACK HIGH
 #define MODE_BLACKBACK LOW
+
+//Uncomment this define for Serial Debug Output
+#define DEBUG
+
+int input_voltage = 0;
+float resistance = 0.0;
 
 //Defines and variables for the LCD 20x04 display using the LiquidCrystal_I2C library
 //Don't actually needs to specify the pins since the nano only has one I2C bus but I include them here just for anyone wondering what pins they are
@@ -43,23 +50,27 @@ int readInput(float active, float inactive, int in_pin) {
 
       //We allow for some lee-way in regards to the valid resistances, it's pretty much never you get an exact value 
       //Typically you get a percentage of accuracy with resistors, we're allowing +/- 1% accuracy here
-      float active_resistance_min = (active - ((0.01 * active) * 10)) + 225;
-      float active_resistance_max = (active + ((0.01 * active) * 10)) + 225; //220ohms is a resistor in the circuit for detecting shorts
-      float inactive_resistance_min = (inactive - ((0.01 * inactive)* 10)) +  225;
-      float inactive_resistance_max = (inactive + ((0.01 * inactive) * 10)) + 225; //220ohms is a resistor in the circuit for detecting shorts   
+      float active_resistance_min = (active - ((0.01 * active) * 10)) + 215;
+      float active_resistance_max = (active + ((0.01 * active) * 10)) + 235; //220ohms is a resistor in the circuit for detecting shorts
+      float inactive_resistance_min = (inactive - ((0.01 * inactive)* 10)) +  215;
+      float inactive_resistance_max = (inactive + ((0.01 * inactive) * 10)) + 235; //220ohms is a resistor in the circuit for detecting shorts   
       
-      int input_voltage = analogRead(in_pin);
+      input_voltage = analogRead(in_pin);
+
+      #ifdef DEBUG
+      Serial.print("Input Voltage is " );
+      Serial.println(input_voltage);
+      #endif
       
-      /*Serial.println("In Voltage is " );
-      Serial.println(input_voltage);*/
-      
-      float resistance = calculateResistance(R1, input_voltage);
+      resistance = calculateResistance(R1, input_voltage);
       if ((resistance <= 300) && (resistance >= 1)) {
         return 3; //Input is Shorted or no resistor There is typically some resistance on the line just due to internal resistance of copper and resistors used in the circuit
         //We allow about 300 ohms for cable length if 300 ohms or less resistance then we got a short
       }
       
-      /*Serial.print("Resistance is ");
+
+      #ifdef DEBUG
+      Serial.print("Resistance is ");
       Serial.println(resistance);
       Serial.print("Active Range is ");
       Serial.print(active_resistance_min);
@@ -68,11 +79,13 @@ int readInput(float active, float inactive, int in_pin) {
       Serial.print("Inactive Range is ");
       Serial.print(inactive_resistance_min);
       Serial.print(" -> ");
-      Serial.println(inactive_resistance_max);*/
+      Serial.println(inactive_resistance_max);
+      #endif
       
-      if ((input_voltage <= 10) && ((isinf(resistance) || resistance > 20000))) {
+      
+      /*if ((input_voltage <= 10) && ((isinf(resistance) || resistance > 20000))) {
         return 6;
-      }
+      }*/
       
       if (input_voltage <= 100)  {
         return 4; //Input either Open or Grounded
@@ -93,13 +106,31 @@ int readInput(float active, float inactive, int in_pin) {
  * 
  * Returns true if the pin is pulled low meaning there is a ground fault or false otherwise. 
  */
-bool checkGroundFault(int digitalPin) {
-  bool groundCheck = digitalRead(digitalPin);
-  //Serial.println(groundCheck);
-  if (groundCheck == HIGH) {
+bool checkGroundFault(int digitalPin1, int digitalPin2) {
+  bool groundCheck = digitalRead(digitalPin1);
+  int groundCheck2 = analogRead(digitalPin2);
+
+  #ifdef DEBUG
+  Serial.print("Ground Check Pin is: ");
+  Serial.println(groundCheck);
+  Serial.print("Ground Check Pin2 is: ");
+  Serial.println(groundCheck2);
+  #endif
+  
+  if ((groundCheck == HIGH) && (groundCheck2 >= 90)) {
+    
+    #ifdef DEBUG
+    Serial.println("Ground Check Returns False");
+    #endif
+    
     return false;
   }
   else {
+
+    #ifdef DEBUG
+    Serial.println("Ground Check Returns TRUE");
+    #endif
+    
     return true;
   }
 }
@@ -132,15 +163,15 @@ void updateOutput(int resistorState, LiquidCrystal_I2C displayObj) {
   }
   else if (resistorState == 4) {
     //Serial.println("Open/Grounded");
-    /*if (checkGroundFault(GROUND_LEAD_IN) == true) {
+    if (checkGroundFault(GROUND_LEAD_IN, GROUND_LEAD_IN_2) == true) {
       //Serial.println("Ground Fault");
-      displayObj.print(" Open/Ground Fault 1");
+      displayObj.print("   Ground Fault    ");
     }
     else {
       //Serial.println("Open Leads");
-      displayObj.print(" Open/Ground Fault 2");
-    }*/
-    displayObj.print("   Ground Fault   ");
+      displayObj.print("    Open Leads     ");
+    }
+    //displayObj.print("   Ground Fault   ");
     
   }
   else if (resistorState == 5) {
@@ -157,7 +188,11 @@ void setup() {
 
   pinMode(MODE_SWITCH_IN, INPUT);
   pinMode(GROUND_LEAD_IN, INPUT_PULLUP); //Set to use pullup resistor, will be pulled low if there's a ground fault.
-  Serial.begin(9600); //Only used in the non-LCD version of the app
+  pinMode(RESISTOR_IN, INPUT_PULLUP);
+  //pinMode(GROUND_LEAD_IN_2, INPUT_PULLUP); //Set to use pullup resistor, will be pulled low if there's a ground fault. 
+  #ifdef DEBUG
+  Serial.begin(9600); //Only used in the non-LCD version of the app and for debugging
+  #endif
 
   //Show the splash screen
   outputDisplay.init();
@@ -186,12 +221,20 @@ void loop() {
 
   outputDisplay.setCursor(0,1);
   if (mode == MODE_BLUEBACK) {
-    //Serial.println("Mode is Blue Back");
+    
+    #ifdef DEBUG
+    Serial.println("Mode is Blue Back");
+    #endif
+
     outputDisplay.print("    Blue Back     ");
     resState = readInput(2000, 1000, RESISTOR_IN);
   }
   else if (mode == MODE_BLACKBACK) {
-    //Serial.println("Mode is Black Back");
+
+    #ifdef DEBUG
+    Serial.println("Mode is Black Back");
+    #endif
+    
     outputDisplay.print("    Black Back    ");
     resState = readInput(500, 1000, RESISTOR_IN);
   }
@@ -200,5 +243,9 @@ void loop() {
   
   updateOutput(resState, outputDisplay);
   delay(500);
+  
+  #ifdef DEBUG
+  delay(3000);
+  #endif
 
 }
